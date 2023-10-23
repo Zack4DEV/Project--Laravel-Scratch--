4859ed50c57634ccb3e91c26ec67cc86e378c1da
El HATIMI Zakaria <73597675+Zack4DEV@users.noreply.github.com>
2023-10-23 00:25:07 +0000

    branch - laravel  - *

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,5 +1,6 @@
 <?php
 
+namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
 
@@ -7,10 +8,8 @@ class ExampleTest extends TestCase
 {
     /**
      * A basic test example.
-     *
-     * @return void
      */
-    public function test_that_true_is_true()
+    public function test_that_true_is_true(): void
     {
         $this->assertTrue(true);
     }
