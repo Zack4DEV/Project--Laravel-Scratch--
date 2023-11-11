cabe3493f66c7482641e48c42b69de7461721081
El HATIMI Zakaria <73597675+Zack4DEV@users.noreply.github.com>
2023-11-11 20:23:41 +0100

    index.php

--- public/index.php
+++ public/index.php
@@ -16,7 +16,7 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-if (file_exists($maintenance = dirname(__FILE__).'/../storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
@@ -31,7 +31,7 @@ if (file_exists($maintenance = dirname(__FILE__).'/../storage/framework/maintena
 |
 */
 
-require dirname(__FILE__).'/../vendor/composer/autoload_real.php';
+require __DIR__.'/../vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -44,7 +44,7 @@ require dirname(__FILE__).'/../vendor/composer/autoload_real.php';
 |
 */
 
-$app = require dirname(__FILE__).'/../bootstrap/app.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
