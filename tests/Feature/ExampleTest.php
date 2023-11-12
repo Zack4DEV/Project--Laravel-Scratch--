6dac49c3c13bde3f70a877cbbb4e79dfcd45446d
El HATIMI Zakaria <73597675+Zack4DEV@users.noreply.github.com>
2023-11-13 00:21:20 +0100

    ExampleTest.php

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,16 +3,16 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use \tests\TestCase as tests_testcase;
+use Tests\TestCase;
 
-class ExampleTest extends tests_testcase
+class ExampleTest extends TestCase
 {
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function test_example()
+    public function test_the_application_returns_a_successful_response()
     {
         $response = $this->get('/');
 
