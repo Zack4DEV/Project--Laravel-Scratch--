4800e3d4f05e0b47f78d405d5f6b18e35d0fe8e6
El HATIMI Zakaria <73597675+Zack4DEV@users.noreply.github.com>
2023-11-17 21:35:13 +0100

    create_MySQL.sql

--- storage/app/public/database/create_MySQL.sql
+++ storage/app/public/database/create_MySQL.sql
@@ -1,34 +1,47 @@
-CREATE DATABASE IF NOT EXISTS `database_hotel` ;
-USE database_hotel;
+-- SQLINES DEMO *** me from config.yml
+
+PRAGMA journal_mode = MEMORY;
+PRAGMA synchronous = OFF;
+PRAGMA foreign_keys = OFF;
+PRAGMA ignore_check_constraints = OFF;
+PRAGMA auto_vacuum = NONE;
+PRAGMA secure_delete = OFF;
+START TRANSACTION;
+
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 CREATE TABLE IF NOT EXISTS `emp_login` (
     `empid` int(100) NOT NULL,
     `Emp_Email` varchar(50) NOT NULL,
     `Emp_Password` varchar(50) NOT NULL,
     PRIMARY KEY (`empid`)
-) ENGINE = MyISAM DEFAULT CHARSET = utf8 COLLATE = utf8_general_ci;
+);
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 CREATE TABLE IF NOT EXISTS `failed_jobs` (
     `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
     `uuid` varchar(191) NOT NULL,
-    `connection` text NOT NULL,
-    `queue` text NOT NULL,
+    `connection` longtext NOT NULL,
+    `queue` longtext NOT NULL,
     `payload` longtext NOT NULL,
     `exception` longtext NOT NULL,
-    `failed_at` timestamp NOT NULL DEFAULT current_timestamp(),
+    `failed_at` datetime NOT NULL DEFAULT current_timestamp(),
     PRIMARY KEY (`id`),
     UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`)
-) ENGINE = MyISAM DEFAULT CHARSET = utf8 COLLATE = utf8_general_ci;
+);
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 CREATE TABLE IF NOT EXISTS `migrations` (
     `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
     `migration` varchar(191) NOT NULL,
     `batch` int(11) NOT NULL,
     PRIMARY KEY (`id`)
-) ENGINE = MyISAM DEFAULT CHARSET = utf8 COLLATE = utf8_general_ci;
+);
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 CREATE TABLE IF NOT EXISTS `password_resets` (
     `email` varchar(191) NOT NULL,
     `token` varchar(191) NOT NULL,
-    `created_at` timestamp NULL DEFAULT NULL,
+    `created_at` datetime NULL DEFAULT NULL,
     KEY `password_resets_email_index` (`email`)
-) ENGINE = MyISAM DEFAULT CHARSET = utf8 COLLATE = utf8_general_ci;
+);
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 CREATE TABLE IF NOT EXISTS `payment` (
     `id` int(30) NOT NULL,
     `Name` varchar(30) NOT NULL,
@@ -45,27 +58,30 @@ CREATE TABLE IF NOT EXISTS `payment` (
     `mealtotal` double(8, 2) NOT NULL,
     `finaltotal` double(8, 2) NOT NULL,
     PRIMARY KEY (`id`)
-) ENGINE = MyISAM DEFAULT CHARSET = utf8 COLLATE = utf8_general_ci;
+);
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 CREATE TABLE IF NOT EXISTS `personal_access_tokens` (
     `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
     `tokenable_type` varchar(191) NOT NULL,
     `tokenable_id` bigint(20) UNSIGNED NOT NULL,
     `name` varchar(191) NOT NULL,
     `token` varchar(64) NOT NULL,
-    `abilities` text DEFAULT NULL,
-    `last_used_at` timestamp NULL DEFAULT NULL,
-    `created_at` timestamp NULL DEFAULT NULL,
-    `updated_at` timestamp NULL DEFAULT NULL,
+    `abilities` longtext DEFAULT NULL,
+    `last_used_at` datetime NULL DEFAULT NULL,
+    `created_at` datetime NULL DEFAULT NULL,
+    `updated_at` datetime NULL DEFAULT NULL,
     PRIMARY KEY (`id`),
     UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
     KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`, `tokenable_id`)
-) ENGINE = MyISAM DEFAULT CHARSET = utf8 COLLATE = utf8_general_ci;
+);
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 CREATE TABLE IF NOT EXISTS `room` (
     `id` int(30) NOT NULL,
     `type` varchar(50) NOT NULL,
     `bedding` varchar(50) NOT NULL,
     PRIMARY KEY (`id`)
-) ENGINE = MyISAM DEFAULT CHARSET = utf8 COLLATE = utf8_general_ci;
+);
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 CREATE TABLE IF NOT EXISTS `roombook` (
     `id` int(10) NOT NULL,
     `Name` varchar(50) NOT NULL,
@@ -81,19 +97,27 @@ CREATE TABLE IF NOT EXISTS `roombook` (
     `nodays` int(50) NOT NULL,
     `stat` varchar(30) NOT NULL,
     PRIMARY KEY (`id`)
-) ENGINE = MyISAM DEFAULT CHARSET = utf8 COLLATE = utf8_general_ci;
+);
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 CREATE TABLE IF NOT EXISTS `signup` (
     `UserID` int(100) NOT NULL,
     `Username` varchar(50) NOT NULL,
     `Email` varchar(50) NOT NULL,
     `Password` varchar(50) NOT NULL,
     PRIMARY KEY (`UserID`)
-) ENGINE = MyISAM DEFAULT CHARSET = utf8 COLLATE = utf8_general_ci;
+);
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 CREATE TABLE IF NOT EXISTS `staff` (
     `id` int(30) NOT NULL,
     `name` varchar(30) NOT NULL,
     `work` varchar(30) NOT NULL,
     PRIMARY KEY (`id`)
-) ENGINE = MyISAM DEFAULT CHARSET = utf8 COLLATE = utf8_general_ci;
-SET FOREIGN_KEY_CHECKS = 1;
+);
 COMMIT;
+
+
+COMMIT;
+PRAGMA ignore_check_constraints = ON;
+PRAGMA foreign_keys = OFF;
+PRAGMA journal_mode = WAL;
+PRAGMA synchronous = NORMAL;
