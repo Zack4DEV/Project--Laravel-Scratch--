e80e434be8b4a8c2e3e9830fa1f42d4b68426497
El HATIMI Zakaria <73597675+Zack4DEV@users.noreply.github.com>
2023-11-17 17:49:51 +0100

    SDashboard.blade.php

--- resources/views/Admins/Admin/Show/SDashboard.blade.php
+++ resources/views/Admins/Admin/Show/SDashboard.blade.php
@@ -1,20 +1,3 @@
-@php
-
-$roombookrow = DB::select("SELECT * FROM roombook");
-$staffrow = DB::select("SELECT * FROM staff");
-$roomrow = DB::select("SELECT * FROM room");
-
-$result = DB::select("SELECT cout,finaltotal FROM payment");
-
-$chart_data = '';
-$tot = 0;
-foreach ($result as $row) {
-$chart_data .= "{ date:'" . $row["cout"] . "', profit:" . $row["finaltotal"] * 10 / 100 . "}, ";
-$tot = $tot + $row["finaltotal"] * 10 / 100;
-}
-$chart_data = substr($chart_data, 0, -2);
-@endphp
-
 <div class="databox">
     <div class="box roombookbox">
         <h2>Total Booked Room</h1>
