381750a51fdccf43be7c18416f8558b6d9ef1a78
El HATIMI Zakaria <73597675+Zack4DEV@users.noreply.github.com>
2023-11-28 22:30:08 +0100

    paymantdelete.php

--- admin/paymantdelete.php
+++ admin/paymantdelete.php
@@ -7,6 +7,6 @@ $deletesql = $conn->prepare("DELETE FROM payment WHERE id = $id");
 $deletesql->execute();
 $result = $deletesql->fetchAll(PDO::FETCH_ASSOC);
 
-header("Location:payment.php");
+header("Location:paymant.php");
 
 ?>
