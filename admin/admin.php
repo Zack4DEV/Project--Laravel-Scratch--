d93b312a9c702fcf8e5d252e32cdb6ef84bf45dc
El HATIMI Zakaria <73597675+Zack4DEV@users.noreply.github.com>
2023-11-28 22:29:53 +0100

    admin.php

--- admin/admin.php
+++ admin/admin.php
@@ -57,7 +57,7 @@ if($usermail == true){
     <div class="mainscreen">
         <iframe class="frames frame1 active" src="./dashboard" frameborder="0"></iframe>
         <iframe class="frames frame2" src="./roombook" frameborder="0"></iframe>
-        <iframe class="frames frame3" src="./payment" frameborder="0"></iframe>
+        <iframe class="frames frame3" src="./paymant" frameborder="0"></iframe>
         <iframe class="frames frame4" src="./room" frameborder="0"></iframe>
         <iframe class="frames frame4" src="./staff" frameborder="0"></iframe>
     </div>
