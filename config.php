12f42310158717f13e2b9d73e09d72ba3e31561c
Zack4DEV <73597675+Zack4DEV@users.noreply.github.com>
2024-03-22 20:02:13 +0000

    branch _ origin

--- config.php
+++ config.php
@@ -3,8 +3,8 @@
 global $conn;
 
 try {
-   $conn = new PDO("mysql:host=localhost;dbname=db", 'root', '');
-    //$conn = new PDO("sqlite:./database/DB.db");
+   //$conn = new PDO("mysql:host=localhost;dbname=db", 'root', '');
+    $conn = new PDO("sqlite:./database/DB.db");
     $conn->SetAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 // echo "Connected Successufully <br />";
 }
